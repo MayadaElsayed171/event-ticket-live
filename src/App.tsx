fb80f378cfc8a66d1c44f466835c26e596ecb781
Mayada Elsayed <mayadaelsayed44@gmail.com>
2026-01-12 16:12:25 +0200

    Add final build for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,41 @@
 // );
 
 // export default App;
+///////////////////////////////////////////////////////////////////
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { Routes, Route } from "react-router-dom";
+
+// import Index from "./pages/Index";
+// import Concerts from "./pages/Concerts";
+// import Matches from "./pages/Matches";
+// import EventDetails from "./pages/EventDetails";
+// import NotFound from "./pages/NotFound";
+
+// // إنشاء Query Client
+// const queryClient = new QueryClient();
+
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <TooltipProvider>
+//       <Toaster />
+//       <Sonner />
+//       <Routes>
+//         <Route path="/" element={<Index />} />
+//         <Route path="/concerts" element={<Concerts />} />
+//         <Route path="/matches" element={<Matches />} />
+//         <Route path="/event/:id" element={<EventDetails />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </TooltipProvider>
+//   </QueryClientProvider>
+// );
+
+// export default App;
+
+
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -43,7 +78,6 @@ import Matches from "./pages/Matches";
 import EventDetails from "./pages/EventDetails";
 import NotFound from "./pages/NotFound";
 
-// إنشاء Query Client
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -63,6 +97,8 @@ const App = () => (
 );
 
 export default App;
+
+
 
 
 
